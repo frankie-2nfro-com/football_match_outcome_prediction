5253eb775261819bfa9b03946e3a4cbce107c885
siuwaikau <siuwaikau@gmail.com>
2022-07-07 13:59:39 +0100

    update football_result.py

--- football_result.py
+++ football_result.py
@@ -1,119 +1,164 @@
-import os
+import re
 import pandas as pd
+import os
 import numpy as np
 from csv import reader
 import plotly.express as px
 import missingno as msno
+import pickle
 
 class DataInterface:
-    def __init__(self, directory):
-        self.dir_error = False
-        self.data = pd.DataFrame()            # store the dataframe
+	def __init__(self, directory):
+		self.dir_error = False
+		self.data = pd.DataFrame()            # store the dataframe
 
-        if not os.path.exists(directory):
-            print("Error: Database directory not found! You have to check if the database folder is exist")
-            self.dir_error = True
-        else:
-            self.loadData(directory)
+		if not os.path.exists(directory):
+			print("Error: Database directory not found! You have to check if the database folder is exist")
+			self.dir_error = True
+		else:
+			self.loadData(directory)
 
-    def loadData(self, directory):
-        # load data files
-        pass
+	def loadData(self, directory):
+		# load data files
+		pass
 
 
 class FootballResultInterface(DataInterface):
-    def loadData(self, dir):
-        # load all directory as league name list
-        self.leagues = [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]
+	def loadData(self, dir):
+		# load pickle and read content
+		self.elo_data = pickle.load(open('./ELO/elo_dict.pkl', 'rb'))
 
-        # loop to open csv
-        for league in self.leagues:
-            league_folder = os.path.join(dir, league)
-            csv_file_for_league = [os.path.join(league_folder, name) for name in os.listdir(league_folder) if name.endswith('.csv')]
-            league_pds = [pd.read_csv(csv_filename, skiprows=[0], names=["Home_Team", "Away_Team", "Result", "Link", "Season", "Round", "League"]) for csv_filename in csv_file_for_league]
-            whole_list_df = pd.concat(league_pds)
-            self.data = pd.concat([self.data, whole_list_df])
+		# load all directory as league name list
+		leagues = [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]
 
-        # Divide result into home_score and away_score
-        df_score =  self.data['Result'].str.extract(r'(\d)-(\d)')
-        self.data.insert(loc=3, column="Home_Score", value=df_score[0].astype('Int64'))
-        self.data.insert(loc=4, column="Away_Score", value=df_score[1].astype('Int64')) 
+		# loop to open csv
+		result_with_goal_sofar_pd = pd.DataFrame()
+		for league in leagues:
+			print("process league: " + league + "...")
+			league_folder = os.path.join(dir, league)
+			csv_file_for_league = [os.path.join(league_folder, name) for name in os.listdir(league_folder) if name.endswith('.csv')]
+			
+			for csv_filename in csv_file_for_league:
+				current_league_season_pd = pd.read_csv(csv_filename, skiprows=[0], names=["Home_Team", "Away_Team", "Result", "Link", "Season", "Round", "League"])
 
-        # calculate score difference
-        df_h_a = self.data["Home_Score"] - self.data["Away_Score"]
-        df_h_a.isna().sum()
+				# Divide result into home_score and away_score
+				df_score =  current_league_season_pd['Result'].str.extract(r'(\d)-(\d)')
+				current_league_season_pd.insert(loc=3, column="Home_Score", value=df_score[0].astype('Int64'))     # use Int64 as it support NaN
+				current_league_season_pd.insert(loc=4, column="Away_Score", value=df_score[1].astype('Int64')) 
 
-        # add home_win, away_win and draw column
-        hw_array = []
-        aw_array = []
-        d_array = []
-        for minus_score in df_h_a:
-            if pd.isna(minus_score):
-                #print(minus_score, type(minus_score))
-                hw_array.append(pd.NA)
-                aw_array.append(pd.NA)
-                d_array.append(pd.NA)
-            else:
-                if minus_score>0:
-                    #print("HOME WIN")
-                    hw_array.append(True)
-                    aw_array.append(False)
-                    d_array.append(False)
-                elif minus_score<0:
-                    #print("AWAY WIN")
-                    hw_array.append(False)
-                    aw_array.append(True)
-                    d_array.append(False)
-                else:
-                    #print("DRAW")
-                    hw_array.append(False)
-                    aw_array.append(False)
-                    d_array.append(True)
+				if len(current_league_season_pd)>0:
+					# get home team and away team total goal so far
+					home_away_total_goal_sofar = current_league_season_pd.apply(self.fillWithTotalGoalSoFar, data=current_league_season_pd, axis=1)
+					goal_so_far_list = np.array(home_away_total_goal_sofar.values.tolist())         # convert to list
+					home_away_total_goal_sofar_pd = pd.DataFrame(goal_so_far_list, columns=["HOME_GOAL_SO_FAR", "AWAY_GOAL_SO_FAR"])    # convert to dataframe
+					current_league_season_pd.insert(loc=5, column="HOME_TOTAL_GOAL_SO_FAR", value=home_away_total_goal_sofar_pd["HOME_GOAL_SO_FAR"]) 
+					current_league_season_pd.insert(loc=6, column="AWAY_TOTAL_GOAL_SO_FAR", value=home_away_total_goal_sofar_pd["AWAY_GOAL_SO_FAR"])     
 
-        self.data.insert(loc=5, column="Home_Win", value=hw_array) 
-        self.data.insert(loc=6, column="Away_Win", value=aw_array) 
-        self.data.insert(loc=7, column="Draw", value=d_array)
+					# merge with ELO
+					result_elo_pd = current_league_season_pd['Link'].apply(self.fillWithELO)   
+					elo_list = np.array(result_elo_pd.values.tolist())
+					elo_df = pd.DataFrame(elo_list, columns=["ELO_HOME", "ELO_AWAY"])
+					current_league_season_pd.insert(loc=8, column="ELO_HOME", value=elo_df["ELO_HOME"]) 
+					current_league_season_pd.insert(loc=9, column="ELO_AWAY", value=elo_df["ELO_AWAY"]) 
 
-        # print statistic
-        print(self.data.info())
+					# get recent performance
+					home_away_recent_perf = current_league_season_pd.apply(self.fillWithRecentPerformance, data=current_league_season_pd, axis=1)
+					perf_list = np.array(home_away_recent_perf.values.tolist())
+					home_away_perf_pd = pd.DataFrame(perf_list, columns=["HOME_LAST_6_GOAL_DIFF", "AWAY_LAST_6_GOAL_DIFF"])
+					current_league_season_pd.insert(loc=7, column="HOME_LAST_6_GOAL_DIFF", value=home_away_perf_pd["HOME_LAST_6_GOAL_DIFF"]) 
+					current_league_season_pd.insert(loc=8, column="AWAY_LAST_6_GOAL_DIFF", value=home_away_perf_pd["AWAY_LAST_6_GOAL_DIFF"]) 
+
+					result_with_goal_sofar_pd = pd.concat([result_with_goal_sofar_pd, current_league_season_pd])
+
+		# export to csv
+		result_with_goal_sofar_pd.to_csv('cleaned_dataset.csv', index=False)
+
+		self.data = result_with_goal_sofar_pd
 
 
-    def getLeagueSeasonDataFrame(self, league, season):
-        return self.data[(self.data["League"]==league) & (self.data["Season"]==season)]
+	def getLeagueSeasonTeamBeforeRoundTotalGoal(self, data, league, season, team, round):
+		# determine home or away and get the score 
+		# get home game of the team
+		home_pd = data[(data["League"]==league) & (data["Home_Team"]==team) & (data["Season"]==season) & (data["Round"]<round)]
+		df_home_score_sofar =  home_pd['Result'].str.extract(r'(\d)-\d')
+		home_total_score = df_home_score_sofar[0].astype('Int64').sum()
 
-    
-    def getLeagueSeasonList(self, league):
-        df = self.data[self.data["League"]==league]
-        return df["Season"].sort_values().unique()
+		# get away game of the team
+		away_pd = data[(data["League"]==league) & (data["Away_Team"]==team) & (data["Season"]==season) & (data["Round"]<round)]
+		df_away_score_sofar =  away_pd['Result'].str.extract(r'\d-(\d)')
+		away_total_score = df_away_score_sofar[0].astype('Int64').sum()
 
-
-    def getLeagueSeasonTeamDataFrame(self, league, season, team):
-        return self.data[(self.data["League"]==league) & ((self.data["Home_Team"]==team) | (self.data["Away_Team"]==team)) & (self.data["Season"]==season)]
-
-
-    def getLeagueTeamCount(self, league, season):
-        league_season_df = self.getLeagueSeasonDataFrame(league, season)
-        return len(league_season_df["Home_Team"].sort_values().unique())
+		# calculate total goals
+		return (home_total_score + away_total_score)
 
 
-    def getLeagueTeamResult(self, league, team, season):
-        league_season_df = self.getLeagueSeasonDataFrame(league, season)
-        return len(league_season_df["Home_Team"].sort_values().unique())
+	def fillWithTotalGoalSoFar(self, record, data):
+		# get home team and away team and round
+		league = record['League']
+		season = record['Season']
+		round = record['Round']
+		hteam = record['Home_Team']
+		ateam = record['Away_Team']
+		
+		home_goal_so_far = self.getLeagueSeasonTeamBeforeRoundTotalGoal(data, league, season, hteam, round)
+		away_goal_so_far = self.getLeagueSeasonTeamBeforeRoundTotalGoal(data, league, season, ateam, round)
+
+		return [home_goal_so_far, away_goal_so_far]
 
 
-    def getTeamCount(self, season):
-        league_season_df = self.data[(self.data["Season"]==season)]
-        return len(league_season_df["Home_Team"].sort_values().unique())
+	def fillWithELO(self, link):
+		if link not in self.elo_data:
+			return [pd.NA, pd.NA]
+		else:
+			return [self.elo_data[link]['Elo_home'], self.elo_data[link]['Elo_away']]
 
 
-    def getLeagueSeasonHomeWinCount(self, league, season):
-        return len(self.data[(self.data["Home_Win"]==True) & (self.data["League"]==league) & (self.data["Season"]==season)])
+	def findRecentPreviousRounds(self, currentRound, limit):
+		if currentRound<=limit:
+			return None
+		else:
+			r = []
+			for l in range(limit):
+				r.append(currentRound - (limit-l))
+			return r
 
-    
-    def getLeagueSeasonHomeWinCount(self, league, season):
-        return len(self.data[(self.data["Home_Win"]==True) & (self.data["League"]==league) & (self.data["Season"]==season)])
+
+	def findLeagueSeasonTeamRecentPreviousRounds(self, data, league, season, team, round):
+		rounds = self.findRecentPreviousRounds(round, 6)         # by definition is 6, can change for optimization
+		if rounds is None:
+			return None
+
+		previous_matches_pd =  data[(data["League"]==league) & ((data["Home_Team"]==team) | (data["Away_Team"]==team)) & (data["Season"]==season) & (data["Round"].isin(rounds))]
+		recent_perf = 0
+		for index, row in previous_matches_pd.iterrows():
+			hteam = row['Home_Team']
+			ateam = row['Away_Team']
+			if hteam==team:
+				recent_perf = recent_perf + (row['Home_Score']-row['Away_Score'])
+			else:
+				recent_perf = recent_perf + (row['Away_Score']-row['Home_Score'])
+
+		return recent_perf
 
 
-    def getResult(league, season):
-        print("Try to get result records of [" + league + "] for season " + str(season))
+	def fillWithRecentPerformance(self, record, data):
+		# get home team and away team and round
+		league = record['League']
+		season = record['Season']
+		round = record['Round']
+		hteam = record['Home_Team']
+		ateam = record['Away_Team']
+		
+		home_team_goal_diff = self.findLeagueSeasonTeamRecentPreviousRounds(data, league, season, hteam, round)
+		away_team_goal_diff = self.findLeagueSeasonTeamRecentPreviousRounds(data, league, season, ateam, round)
 
+		return [home_team_goal_diff, away_team_goal_diff]
+
+
+
+
+
+if __name__ == '__main__':
+	result_data = FootballResultInterface('./Results')
+
+	print(result_data.data)
